35699121871cfd5583dd6166f74b1d983daca5a1
Diego <Diego@Dell>
2013-09-25 13:00:27 -0300

    ok

---
+++ swing/src/swing/Swing.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swing;
+
+/**
+ *
+ * @author Diego
+ */
+public class Swing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
